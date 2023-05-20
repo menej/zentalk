cf7ac02b4161086ebaa08bc1bacff6cfe3261029
Enej <mastnak.enej@gmail.com>
2023-05-20 12:13:59 +0200

    Implemented showLoginForm function and login function

--- controller/UserController.php
+++ controller/UserController.php
@@ -1,14 +1,49 @@
 <?php
 
+require_once("model/UserDB.php");
+require_once("ViewHelper.php");
+
 class UserController
 {
 
-    public static function showLoginForm(): void
+    public static function showLoginForm($data = [], $errors = []): void
     {
-        if (empty($_SESSION["user"])) {
-            ViewHelper::render("view/users/user-login-form.php");
-        } else {
-            ViewHelper::redirect(BASE_URL . "home/home-page.php");
+        if (!empty($_SESSION["user"])) {
+            ViewHelper::redirect(BASE_URL . "home");
+            return;
         }
+
+        if (empty($errors)) {
+            $errors = [
+                "username" => "",
+                "password" => "",
+                "incorrect" => ""
+            ];
+        }
+
+        $vars = ["errors" => $errors];
+
+        ViewHelper::render("view/users/user-login-form.php", $vars);
+    }
+
+    // POST request
+    public static function login()
+    {
+        if (UserDB::validLoginAttempt($_POST["username"], $_POST["password"])) {
+            $_SESSION["user"] = $_POST["username"];
+
+            ViewHelper::redirect(BASE_URL . "home");
+        } else {
+            //ViewHelper::render("view/user-login-form.php", [
+             //   "errorMessage" => "Invalid username or password."
+            //]);
+            echo "hacker?<br>";
+        }
+    }
+
+    public static function profile()
+    {
+        unset($_SESSION["user"]);
+        ViewHelper::redirect(BASE_URL . "home");
     }
 }
