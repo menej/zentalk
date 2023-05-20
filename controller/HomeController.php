18e51d5223d4e57a2897220c23c196e30a0b1838
Enej <mastnak.enej@gmail.com>
2023-05-20 18:11:54 +0200

    Implemented showing of the latest posts in the HomeController.php

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -1,10 +1,21 @@
 <?php
 
+require_once("model/PostDB.php");
+require_once("model/UserDB.php");
+
 class HomeController
 {
     // Show home page (GET: index)
     public static function index()
     {
-        ViewHelper::render("view/home/home-page.php");
+        // Get 5 latest posts
+        $data = PostDB::getFiveLatest();
+
+        foreach ($data as &$post) {
+            $user = UserDB::getUser($post["uid"]);
+            $post["user"] = $user;
+        }
+
+        ViewHelper::render("view/home/home-page.php", ["posts" => $data]);
     }
 }
