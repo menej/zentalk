ad8b4e7d8ae4d6e5f5e369b967be77b5dbc902fa
Enej <mastnak.enej@gmail.com>
2023-05-20 18:14:39 +0200

    Updated UI of the login form in user-login-form.php

--- view/users/user-login-form.php
+++ view/users/user-login-form.php
@@ -13,12 +13,28 @@
 <?php include(INC_URL . "navbar.php"); ?>
 
 
-<?php if($errors["incorrect"]): ?>
-<p class="text-danger"><?= $errors["incorrect"] ?></p>
+<?php if ($errors["incorrect"]): ?>
+    <p class="text-danger"><?= $errors["incorrect"] ?></p>
 
 <?php endif; ?>
 
-<form action="<?= BASE_URL . "user/login" ?>" method="POST">
+
+<div class="container-lg my-5">
+    <form action="<?= BASE_URL . "user/login" ?>" method="POST">
+        <div class="form-floating mb-5">
+            <input type="text" id="username" name="username" class="form-control" placeholder="Place username here" required autofocus>
+            <label for="username" class="form-label">Username</label>
+        </div>
+
+        <div class="form-floating mb-5">
+            <input type="password" id="password" name="password" class="form-control" placeholder="Place password here" required>
+            <label for="password" class="form-label">Password</label>
+        </div>
+
+        <div class="d-grid gap-2">
+            <button class="btn btn-primary btn-outline-light fs-5">Login</button>
+        </div>
+        <!--
     <label for="username">Username:
         <input id="username" type="text" name="username" autocomplete="off" required autofocus>
         <span class="text-danger"><?= $errors["username"] ?></span>
@@ -28,10 +44,21 @@
         <span class="text-danger"><?= $errors["password"] ?></span>
     </label><br>
     <button>Login in</button>
-</form>
+     -->
+    </form>
 
-<a href="">Forgot password</a>
-<a href="<?= BASE_URL . "user/register" ?>">New user</a>
+    <div class="row mt-4">
+        <div class="col-sm-6 col-12 text-sm-end">
+            <a href="" class="fs-5 link-success">Forgot password</a>
+        </div>
+        <div class="col-sm-6 col-12 text-sm-start">
+            <a href="<?= BASE_URL . "user/register" ?>" class="fs-5 link-success">New user</a>
+        </div>
+
+
+    </div>
+
+</div>
 
 
 </body>
