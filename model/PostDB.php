ce36dcd1ccc4e7aa93f0578a61eb6105dc64b0b9
Enej <mastnak.enej@gmail.com>
2023-05-20 12:14:59 +0200

    Implemented get and getAll functions inside PostDB.php

--- model/PostDB.php
+++ model/PostDB.php
@@ -5,4 +5,38 @@ require_once "DBInit.php";
 class PostDB
 {
 
+    public static function get(mixed $id)
+    {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("
+            SELECT pid, uid, title, content, rating, post_date
+            FROM post 
+            WHERE pid = :id
+        ");
+        $statement->bindParam(":id", $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        $book = $statement->fetch();
+
+        if ($book != null) {
+            return $book;
+        } else {
+            // TODO: catch the error and show 404 page
+            throw new InvalidArgumentException("No record with id $id");
+        }
+    }
+
+    public static function getAll(): bool|array
+    {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("
+            SELECT pid, uid, title, content, rating 
+            FROM post;
+        ");
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
 }
