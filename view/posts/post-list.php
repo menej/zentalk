3f502020a9d7910015bd7b1f91dc21c258c34290
Enej <mastnak.enej@gmail.com>
2023-05-20 12:15:34 +0200

    Implemented post list

---
+++ view/posts/post-list.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @var array $posts
+ */
+?>
+
+<?php include(INC_URL . "header.php") ?>
+
+<title>All Posts</title>
+
+<style>
+    .clickable-div {
+      cursor: pointer;
+    }
+</style>
+
+<body id="bootstrap-overrides">
+
+<?php include(INC_URL . "navbar.php"); ?>
+
+
+<div class="container-lg my-5">
+    <div class="row mb-4">
+        <div class="col-12 text-center">
+            <h1 class="display-3 border-bottom">Check out all the posts</h1>
+        </div>
+    </div>
+
+    <?php foreach ($posts as $post): ?>
+        <div data-url="post/<?= $post["pid"] ?>" class="row mb-4 clickable-div user-select-none">
+            <div class="col-2 bg-secondary d-flex align-items-center">
+                <p class="fs-4 fw-bold my-auto"><?= $post["user"]["username"] ?></p>
+            </div>
+            <div class="col-10 bg-primary ">
+                <p class="fs-4 text-white my-auto"><?= $post["title"] ?></p>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
+
+<script src="<?= JS_URL . "post-list.js" ?>"></script>
+
+</body>
+
+
